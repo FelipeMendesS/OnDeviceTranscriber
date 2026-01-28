1e65bfdcdc1e738880ed97550013b12f96c4dc8f
Felipe Mendes dos Santos <Felipe2855@gmail.com>
2026-01-28 01:07:43 -0300

    Initial Commit

---
+++ OnDeviceTranscriber/OnDeviceTranscriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnDeviceTranscriberApp.swift
+//  OnDeviceTranscriber
+//
+//  Created by Felipe Mendes dos Santos on 28/01/26.
+//
+
+import SwiftUI
+
+@main
+struct OnDeviceTranscriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
